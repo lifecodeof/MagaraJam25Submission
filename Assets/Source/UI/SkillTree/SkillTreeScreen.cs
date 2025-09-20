d7f91dcd3fe950d4042fba8781e754193fe8e562
Semih Koyuncu <42251538+lifecodeof@users.noreply.github.com>
2025-09-20 19:22:30 +0300

    improved skill tree screen

--- Assets/Source/UI/SkillTree/SkillTreeScreen.cs
+++ Assets/Source/UI/SkillTree/SkillTreeScreen.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using R3;
 using UnityEngine;
@@ -8,17 +9,48 @@ class SkillTreeCanvas : MonoBehaviour
     private Canvas canvas;
 
     [field: SerializeField]
+    public float AnimationDuration { get; private set; } = 0.5f;
+
+    [field: SerializeField]
     public SerializableReactiveProperty<bool> IsOpen { get; private set; }
+
+    private Vector3 canvasOriginalScale;
+    private RectTransform panelRectTransform;
 
     void Awake()
     {
-        IsOpen.Subscribe(isOpen =>
+        canvasOriginalScale = canvas.transform.localScale;
+        panelRectTransform = canvas.transform.GetChild(0).GetComponent<RectTransform>();
+
+        IsOpen.DistinctUntilChanged().Subscribe(isOpen =>
         {
-            canvas.gameObject.SetActive(isOpen);
-            // TODO: Tween
+            if (isOpen)
+                CloseToOpen();
+            else
+                OpenToClose();
 
             // Pause the game when the skill tree is open
             Time.timeScale = isOpen ? 0f : 1f;
         });
+    }
+
+    private void CloseToOpen()
+    {
+        canvas.gameObject.SetActive(true);
+        panelRectTransform
+            .DOScale(canvasOriginalScale, AnimationDuration)
+            .SetEase(Ease.OutBack)
+            .From(Vector3.zero, true)
+            .SetUpdate(true); // Ignore timeScale
+    }
+
+    private void OpenToClose()
+    {
+        panelRectTransform
+            .DOScale(Vector3.zero, AnimationDuration)
+            .SetEase(Ease.InBack)
+            .From(canvasOriginalScale, true)
+            .OnComplete(() => canvas.gameObject.SetActive(false))
+            .SetUpdate(true); // Ignore timeScale
     }
 }
