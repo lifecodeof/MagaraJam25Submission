24bf26474d997f95e7907ce70674c20231f69430
p1r1 <ofarukmarasli@gmail.com>
2025-09-21 14:40:00 +0300

    background

---
+++ Assets/Source/BackgroundParallax.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, Of)
+    }
+}
